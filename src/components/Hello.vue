fd9db7e66c4d726f1f754e5cad43ca989383f8d2
e <36460092@qq.com>
2017-05-19 17:22:53 +0800

    测试cjgiser

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://vuejs.org" target="_blank">测试cjgiser</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
