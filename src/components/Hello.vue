c815b54704b7a2ac4f16e4d336ddaa84ff201661
liangyuanyuan <lvyweb@163.com>
2017-05-22 14:04:59 +0800

    this is a test

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,7 +4,7 @@
     <h2>Essential Links</h2>
     <h2>stark's test</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">测试测试测试</a></li>
+      <li><a href="https://vuejs.org" target="_blank">测试测试测试lvyweb</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
