f73df952864bca9deb45db9e9ecabd29b2a8b8ab
iuunhao <html.5@foxmail.com>
2017-05-25 21:36:01 +0800

    解决冲突

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,26 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <h2>stark's test</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">再次测试cjgiser</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-=======
     <div class="hello">
         <h1>{{ msg }}</h1>
         <h2>Essential Links</h2>
@@ -79,8 +57,6 @@
 
         <component v-bind:is="currentComponent" message="hade"></component>
     </div>
-
->>>>>>> develop
 </template>
 
 <script>
