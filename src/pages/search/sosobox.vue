3ebaf4fd60fe2c98435142d686b1c0ad6427e081
yang bo <yangbo@yangdeiMac.local>
2017-06-06 19:44:22 +0800

    xxxx

--- src/pages/search/sosobox.vue
+++ src/pages/search/sosobox.vue
@@ -1,7 +1,30 @@
 <template>
 
 <div class="sosobox-container">
-
+    <div class="input">
+        <input type="text">
+        <icon></icon>
+    </div>
+    <div class="cancel"></div>
 </div>
 
+
 </template>
+
+<script>
+import icon from 'components/icon'
+
+export default {
+    name: 'sosobox',
+    components: { icon },
+    data () {
+        return {
+            keyword: ''
+        }
+    },
+    mounted () {
+        console.log('xxx')
+    }
+}
+
+</script>
